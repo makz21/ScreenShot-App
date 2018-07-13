27fb2396818a619c48f85367504d9b9dee9f4a23
makz21 <209444@edu.p.lodz.pl>
2018-07-13 02:33:56 +0200

    folder with ss

--- src/sample/MenuController.java
+++ src/sample/MenuController.java
@@ -1,4 +1,17 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import java.io.IOException;
+
+
 public class MenuController {
+    public Button openFolderWithSS;
+
+    @FXML
+    public void openContainingFolder() throws IOException {
+        Runtime.getRuntime().exec("explorer C:\\Users\\makz\\Desktop\\ScreenShot-App\\Screenshots");
+        System.out.println("folder");
+    }
+
 }
