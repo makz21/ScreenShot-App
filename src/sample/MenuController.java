2a06d08f110ffca3cd529ae19eacadae1c054d70
makz21 <209444@edu.p.lodz.pl>
2018-07-13 02:47:18 +0200

    folder with ss

--- src/sample/MenuController.java
+++ src/sample/MenuController.java
@@ -2,11 +2,17 @@ package sample;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+
 import java.io.IOException;
 
 
 public class MenuController {
+    // public Button takeSS;
     public Button openFolderWithSS;
+    // public Button takeAndUploadSS;
+    // public Button editSS;
+    // public Button coppySS;
+    // public Button copySSUrl
 
     @FXML
     public void openContainingFolder() throws IOException {
